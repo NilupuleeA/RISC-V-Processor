5d192dc8af2be0b4d2d494bfa189eb58d0dc1ecc
Erandee Jayathilaka <erandeejayathilaka@gmail.com>
2025-01-04 01:26:07 +0530

    Update AdderBEQ.sv

--- Single_Cycle/AdderBEQ.sv
+++ Single_Cycle/AdderBEQ.sv
@@ -1,8 +1,8 @@
 module AdderBEQ(
     input logic [31:0] Addr,
-    input logic [31:0] Extended_imm_out,
+    input logic [31:0] Extended_imm,
     output logic [31:0] NextAddr
 );
 
-    assign NextAddr = Addr + (Extended_imm_out<<1);
+    assign NextAddr = Addr + (Extended_imm<<1);
 endmodule
